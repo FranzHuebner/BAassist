f927863b145cbb35e3dcc9d70c4ed52a77f2d64f
RichyWalter <richardwalter@web.de>
2017-05-16 00:18:16 +0200

    Added Navigation Bar

--- app/src/main/java/edu/ba/baassist/MainActivity.java
+++ app/src/main/java/edu/ba/baassist/MainActivity.java
@@ -1,13 +1,22 @@
 package edu.ba.baassist;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        startActivity(new Intent(MainActivity.this, NavigationActivity.class));
+}
 }
