0602047a930670898658ddd5c193e9595a00daf3
RichyWalter <richardwalter@web.de>
2017-05-22 22:31:38 +0200

    added BA Wiki

--- app/src/main/java/edu/ba/baassist/TimetableActvity.java
+++ app/src/main/java/edu/ba/baassist/TimetableActvity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 public class TimetableActvity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,8 @@ public class TimetableActvity extends BaseActivity {
 //        RelativeLayout.LayoutParams lp =new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
 //        addContentView(findViewById(R.id.nav_timetable), lp);
         LayoutInflater inflater = getLayoutInflater();
-        getWindow().addContentView(inflater.inflate(R.layout.activity_navigation, null), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+        getWindow().addContentView(inflater.inflate(R.layout.activity_navigation, null, true), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
+
     }
 }
--- app/src/main/java/edu/ba/baassist/WikiActivity.java
+++ app/src/main/java/edu/ba/baassist/WikiActivity.java
@@ -2,12 +2,27 @@ package edu.ba.baassist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 
-public class WikiActivity extends AppCompatActivity {
+public class WikiActivity extends BaseActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiki);
+
+        LayoutInflater inflater = getLayoutInflater();
+        getWindow().addContentView(inflater.inflate(R.layout.activity_navigation, null), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(false);
+        webView.loadUrl("https://raw.githubusercontent.com/FranzHuebner/BAassist/master/ba_wiki_host");
+
+
     }
 }
