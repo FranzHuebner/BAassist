4a3eea2fd85f3e336dbf6965030bdf99d868ba55
RichyWalter <richardwalter@web.de>
2017-06-11 02:15:25 +0200

    new drawer layout+icons

--- app/src/main/java/edu/ba/baassist/TimetableActvity.java
+++ app/src/main/java/edu/ba/baassist/TimetableActvity.java
@@ -10,6 +10,13 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TimetableActvity extends BaseActivity {
 
 
@@ -26,4 +33,17 @@ public class TimetableActvity extends BaseActivity {
         navigationView.setNavigationItemSelectedListener(this);
 
     }
+
+//    private void getTimetable() throws IOException {
+//        URL url = new URL("http://www.android.com/");
+//        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//        try {
+//            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//            readStream(in);
+//        } finally {
+//            urlConnection.disconnect();
+//        }
+
+
+
 }
