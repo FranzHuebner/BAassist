b83f8631ceb308097dd2f7664a7fd681b30598a5
RichyWalter <richardwalter@web.de>
2017-06-16 01:46:55 +0200

    added list view for timetable implemented complete Navigation added About Fragment with content some layout changes

---
+++ app/src/main/java/edu/ba/fragments/SettingsFragment.java
@@ -0,0 +1,28 @@
+package edu.ba.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.ba.baassist.R;
+
+/**
+ * Created by richa on 16.06.2017.
+ */
+
+public class SettingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+
+        return rootView;
+    }
+}
