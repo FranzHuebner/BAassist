21d24035715c407691e418d5d19bd6cdb443be87
RichyWalter <richardwalter@web.de>
2018-05-15 21:40:07 +0200

    added new json parser for timetable items (filter isn´t working yet)

--- app/src/main/java/edu/ba/fragments/MainFragment.java
+++ app/src/main/java/edu/ba/fragments/MainFragment.java
@@ -3,21 +3,29 @@ package edu.ba.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import edu.ba.baassist.ConnAdapter;
 import edu.ba.baassist.R;
 import edu.ba.baassist.TimetableAdapter;
 import edu.ba.baassist.TimetableItem;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Standard fragment which is the base of the Fragment-Manager.
@@ -37,6 +45,7 @@ public class MainFragment extends Fragment{
 
         String output= ConnAdapter.getUserCal();                     // Get data as string.
 
+        /*/
         String[] timeTableData = output.split("title");             //Array which splits the output string in the different elements.
 
 
@@ -51,9 +60,56 @@ public class MainFragment extends Fragment{
         }
 
         timeTableData = groupFilter(timeTableData, userFilter);        //ConnAdapter.getUserFilter()
+        /*/
 
         ArrayList<Object> list = new ArrayList<>();                 //List which will be displayed.
 
+        try {
+            // Getting JSON Array node
+            JSONArray timeTableContent =new JSONArray(output);
+
+            // looping through All timetable Items
+            for (int i = 0; i < timeTableContent.length(); i++) {
+                JSONObject t = timeTableContent.getJSONObject(i);
+
+                String subject = t.getString("title");
+                String teacher = t.getString("instructor");
+                String begin = t.getString("start");
+                String end = t.getString("end");
+                String room= t.getString("room");
+
+                int beginOfLessonInt = convertTimeStringToInteger(begin);        //Integer of start time.
+                if(beginOfLessonInt>= actTime -5500){                                //Start time of the displayed list.
+
+                    Date timetableDate = ConnAdapter.convertUnixtoNormalDate(beginOfLessonInt);
+
+                    if(ConnAdapter.setTimeToMidnight(timetableDate).after(ConnAdapter.setTimeToMidnight(actDate))){
+                        actDate = timetableDate;
+                        String headerDate = ConnAdapter.convertUnixtoNormalDateString(beginOfLessonInt);
+                        list.add(headerDate);                                           //Add a new header when a new day starts.
+                    }
+
+                    list.add(new TimetableItem(subject, teacher.replace("instructor", ""), ConnAdapter.convertUnixtoNormalTimeString(convertTimeStringToInteger(begin)) + "\n" + ConnAdapter.convertUnixtoNormalTimeString(convertTimeStringToInteger(end)) + "\n" + room));   //Add new Element
+                }
+
+                /*/ tmp hash map for single contact
+                HashMap<String, String> contact = new HashMap<>();
+
+                // adding each child node to HashMap key => value
+                contact.put("id", id);
+                contact.put("name", name);
+                contact.put("email", email);
+                contact.put("mobile", mobile);
+
+                // adding contact to contact list
+                contactList.add(contact);
+                /*/
+            }
+        } catch (final JSONException e) {
+            Log.e(TAG, "Json parsing error: " + e.getMessage());
+            }
+
+/*/
         for(int i=1; i<timeTableData.length; i++){
 
             String[] temp=timeTableData[i].split(",");      //Split data to work with the single elements.
@@ -80,7 +136,7 @@ public class MainFragment extends Fragment{
                 list.add(new TimetableItem(subject, teacher.replace("instructor", ""), ConnAdapter.convertUnixtoNormalTimeString(convertTimeStringToInteger(begin)) + "\n" + ConnAdapter.convertUnixtoNormalTimeString(convertTimeStringToInteger(end)) + "\n" + room));   //Add new Element
             }
         }
-
+/*/
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
